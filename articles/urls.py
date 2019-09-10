a8254b8892bba93acbe123aae255d6ae7a0ab3c9
ssshhh0402 <ssshhh0402@naver.com>
2019-09-10 11:34:32 +0900

    20190910|URL nameSpace

--- articles/urls.py
+++ articles/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
+
+app_name = 'articles'
 urlpatterns = [
-    path('', views.index),
-    path('new/', views.new),
-    path('create/', views.create),
-    path('<int:article_pk>/', views.detail),
-    path('delete/<int:article_pk>', views.delete),
-    path('soojung/<int:article_pk>', views.soojung),
-    path('update/<int:article_pk>', views.update),
+    path('', views.index, name='index'),
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
+    path('<int:article_pk>/', views.detail, name='detail'),
+    path('delete/<int:article_pk>', views.delete, name='delete'),
+    path('soojung/<int:article_pk>', views.soojung, name='soojung'),
+    path('update/<int:article_pk>', views.update, name='update'),
 ]
