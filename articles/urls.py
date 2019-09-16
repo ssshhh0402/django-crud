589d011f555718749ac4ef43f104b7294942d698
ssshhh0402 <ssshhh0402@naver.com>
2019-09-16 10:29:50 +0900

    20190916

--- articles/urls.py
+++ articles/urls.py
@@ -4,10 +4,9 @@ from . import views
 app_name = 'articles'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('new/', views.new, name='new'),
     path('create/', views.create, name='create'),
     path('<int:article_pk>/', views.detail, name='detail'),
     path('delete/<int:article_pk>', views.delete, name='delete'),
-    path('soojung/<int:article_pk>', views.soojung, name='soojung'),
+  
     path('update/<int:article_pk>', views.update, name='update'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Article 
+from django.views.decorators.http import require_POST
 # Create your views here.
 def index(request):
     articles = Article.objects.all()
@@ -8,44 +9,46 @@ def index(request):
     }
     return render(request, 'articles/index.html', context)
 
-def new(request):
-    return render(request, 'articles/new.html')
+
 
 def create(request):
-    in_data = request.POST
-    article = Article.objects.create(title=in_data.get('title'), content=in_data.get('content'))
-    # a = Article.objects.order_by('id')
-    context = {
-        'data' : article
-    }
-    return render(request, 'articles/create.html', context)
-    #return render(request, f'/articles/{article.id}/', context)
-    #return redirect('/articles/')
+    if request.method == 'POST':
+        in_data = request.POST
+        article = Article.objects.create(title=in_data.get('title'), content=in_data.get('content'))
+        context = {
+            'data' : article
+        }
+        return render(request, 'articles/create.html', context)
+  
+    else:
+         return render(request, 'articles/new.html')
+
+def delete(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    if request.method == 'POST':
+        article.delete()
+        return redirect('articles:index')
+    else:
+        return redirect('articles:detail', article.pk)
 
 
 def detail(request, article_pk):
     article = Article.objects.get(pk=article_pk)
     context = {
-      'article' : article
+        'article' : article
     }
     return render(request, 'articles/detail.html', context)
 
-def delete(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
-    article.delete()
-    return redirect('/articles')
-
-def soojung(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
-    context = {
-        'article' : article
-    }
-    return render(request, 'articles/update.html', context)
 
 def update(request, article_pk):
-    in_data = request.POST
     article = Article.objects.get(pk=article_pk)
-    article.content = in_data.get('content')
-    article.save()
-    #return redirect('/articles')
-    return redirect(f'/articles/{article.id}')
+    if request.method == 'POST':
+        in_data = request.POST
+        article.content = in_data.get('content')
+        article.save()
+        return redirect(f'articles:detail',article_pk)
+    else:
+        context = {
+            'article' : article
+        }
+        return render(request, 'articles/update.html', context)
