cf1e7061f85784f2bba04bf5602abb651a749654
ssshhh0402 <ssshhh0402@naver.com>
2019-09-16 10:43:17 +0900

    20190916| Comment(1:N)

--- articles/admin.py
+++ articles/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Article
+from .models import Article, Comment
 
 
 
@@ -9,4 +9,9 @@ from .models import Article
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'content', 'created_at', 'updated_at')
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'article')
+
+
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment, CommentAdmin)
