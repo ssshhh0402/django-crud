a64011d437dc695d24c5dc88f05184855facf6dd
ssshhh0402 <ssshhh0402@naver.com>
2019-09-17 11:17:55 +0900

    16| Create Update Logic

--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Article,Comment 
 from django.views.decorators.http import require_POST
 from django.contrib import messages
+from .forms import ArticleForm
+from IPython import embed
 # Create your views here.
 def index(request):
     articles = Article.objects.all()
@@ -14,15 +16,58 @@ def index(request):
 
 def create(request):
     if request.method == 'POST':
-        in_data = request.POST
-        article = Article.objects.create(title=in_data.get('title'), content=in_data.get('content'))
-        context = {
-            'data' : article
-        }
-        return render(request, 'articles/create.html', context)
+        article_form = ArticleForm(request.POST)
+        embed()
+        if article_form.is_valid():
+            title = article_form.cleaned_data.get('title')
+            content = article_form.cleaned_data.get('content')
+            
+            #article = Article.objects.create('title'=title, content=content)
+            article = Article()
+            article.title=  title
+            article.content = content
+            article.save()
+            return redirect('articles:detail', article.pk)
+    #     else:
+    #         article_form = ArticleForm()
+    # context = {
+    #     'data' : article
+    # }
+    #     return render(request, 'articles/create.html', context)
   
     else:
-         return render(request, 'articles/new.html')
+        article_form = ArticleForm()
+        context = {
+            'article_form': article_form
+        }
+        return render(request, 'articles/new.html', context)
+
+
+def update(request, article_pk):
+    #request = POST => 검증 및 저장
+    article = Article.objects.get(pk=article_pk)
+    if request.method == 'POST':  
+        article_form = ArticleForm(request.POST)
+        if article_form.is_valid():
+            #검증 성공하면 저장
+            content= article_form.cleaned_data.get('content')
+            article.content = content
+            article.save()
+            return redirect('articles:detail', article_pk)
+    # request = GET => Form 전달
+    else:
+        article_form = ArticleForm()
+        initial = {
+            'title' : article.title,
+            'content' : article.content
+        }
+    context = {
+        'article_form' : article_form
+    }
+    return render(request, 'articles/update.html', context)
+    # GET => 비어있는 Form 전달
+    # POST => 검증 실패시 에러메세지와 입력값 채워진 Form context
+
 
 def delete(request, article_pk):
     article = Article.objects.get(pk=article_pk)
@@ -44,18 +89,7 @@ def detail(request, article_pk):
     return render(request, 'articles/detail.html', context)
 
 
-def update(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
-    if request.method == 'POST':
-        in_data = request.POST
-        article.content = in_data.get('content')
-        article.save()
-        return redirect(f'articles:detail',article_pk)
-    else:
-        context = {
-            'article' : article
-        }
-        return render(request, 'articles/update.html', context)
+
 def comment_create(request, article_pk):
     comment = Comment()
     comment.content = request.POST.get('comment')
@@ -67,8 +101,8 @@ def comment_create(request, article_pk):
 
 def c_delete(request, comment_pk):
     comment = Comment.objects.get(id=comment_pk)
-    
+
     article_pk = comment.article_id
     comment.delete()
-    messages.add_message(request, messages.INFO, '삭제되었습니다')
+    messages.add_message(request, messages.WARNING, '삭제되었습니다')
     return redirect('articles:detail', article_pk)
