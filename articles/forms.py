f27b62fa6a52046081a69ba153bfed38cd68f0be
ssshhh0402 <ssshhh0402@naver.com>
2019-09-23 10:41:25 +0900

    20| Change comment form to modelform

--- articles/forms.py
+++ articles/forms.py
@@ -1,15 +1,16 @@
 from django import forms
-from .models import Article
+from .models import Article, Comment
 class ArticleForm(forms.ModelForm):
-    # title = forms.CharField(
-    #     max_length=1,
-    #     label='제목',
-    #     widget=forms.TextInput(
-    #         attrs={
-    #             'placeholder': '제목을 입력바랍니다.'
-    #         }
-    #     )
-    # )    
+    title = forms.CharField(
+        max_length=140,
+        label='제목',
+        help_text='140자 이내로 작성 바랍니다.',
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': '제목을 입력바랍니다.'
+            }
+        )
+    )    
     class Meta :
         model = Article
         fields = '__all__'
@@ -21,3 +22,8 @@ class ArticleForm(forms.ModelForm):
         #     )
         # }
         #위젯 설정은 위에 있는 두가지 방법 모두 아무거로나 가능
+
+class CommentForm(forms.ModelForm):
+    class Meta :
+        model = Comment
+        exclude = ('article',)
--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Article,Comment 
 from django.views.decorators.http import require_POST
 from django.contrib import messages
-from .forms import ArticleForm
+from .forms import ArticleForm,CommentForm
 
 # Create your views here.
 def index(request):
@@ -19,32 +19,19 @@ def create(request):
         article_form = ArticleForm(request.POST)
       
         if article_form.is_valid():
-            # title = article_form.cleaned_data.get('title')
-            # content = article_form.cleaned_data.get('content')
-            # article = Article()
-            # article.title=  title
-            # article.content = content
-            # article.save()
             article = article_form.save()
             return redirect('articles:detail', article.pk)
-    #     else:
-    #         article_form = ArticleForm()
-    # context = {
-    #     'data' : article
-    # }
-    #     return render(request, 'articles/create.html', context)
-  
     else:
         article_form = ArticleForm()
-        context = {
-            'article_form': article_form
-        }
-        return render(request, 'articles/new.html', context)
+    context = {
+        'article_form': article_form
+    }
+    return render(request, 'articles/new.html', context)
 
 
 def update(request, article_pk):
     #request = POST => 검증 및 저장
-    article = Article.objects.get(pk=article_pk)
+    article =get_object_or_404(Article, pk=article_pk)
     if request.method == 'POST':  
         article_form = ArticleForm(request.POST, instance=article)
         if article_form.is_valid():
@@ -63,13 +50,13 @@ def update(request, article_pk):
     context = {
         'article_form' : article_form
     }
-    return render(request, 'articles/update.html', context)
+    return render(request, 'articles/form.html', context)
     # GET => 비어있는 Form 전달
     # POST => 검증 실패시 에러메세지와 입력값 채워진 Form context
 
 
 def delete(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
+    article =get_object_or_404(Article, pk=article_pk)
     if request.method == 'POST':
         article.delete()
         return redirect('articles:index')
@@ -78,29 +65,44 @@ def delete(request, article_pk):
 
 
 def detail(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
+    article =get_object_or_404(Article, pk=article_pk)
     comment = article.comment_set.all()
-    # comment = Comment.objects.get(article_id=article_pk).all()
+    comment_form = CommentForm()
     context = {
         'article' : article,
-        'comment' : comment
+        'comment' : comment,
+        'comment_form' : comment_form
     }
     return render(request, 'articles/detail.html', context)
 
 
 
 def comment_create(request, article_pk):
-    comment = Comment()
-    comment.content = request.POST.get('comment')
-    comment.article_id= article_pk
-    comment.save()
-    messages.add_message(request, messages.INFO, '등록되었습니다')
+    article = get_object_or_404(Article, pk=article_pk)
+    # 1. model.form에 사용자 입력값
+    comment_form = CommentForm(request.POST)
+    # 2. 검증
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.article = article
+        comment.save()
+    # 3. return redirect
+    else:
+        messages.success(request, '댓글이 형식에 맞지 않습니다')
     return redirect('articles:detail', article_pk)
+
+
+
+    # comment = Comment()
+    # comment.content = request.POST.get('comment')
+    # comment.article_id= article_pk
+    # comment.save()
+    # messages.add_message(request, messages.INFO, '등록되었습니다')
+    # return redirect('articles:detail', article_pk)
 
 
 def c_delete(request, comment_pk):
     comment = Comment.objects.get(id=comment_pk)
-
     article_pk = comment.article_id
     comment.delete()
     messages.add_message(request, messages.WARNING, '삭제되었습니다')
