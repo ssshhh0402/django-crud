da292c07c9f77526bee8cbbec07d37ea8464d6af
ssshhh0402 <ssshhh0402@naver.com>
2019-09-23 15:26:43 +0900

    22| 사진 보정

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from imagekit.models import ProcessedImageField, ImageSpecField
+from imagekit.processors import ResizeToFill
 # Create your models here.
 # 1. 모델(스키마) 정의
 # 데이터베이스 테이블을 정의하고,
@@ -15,14 +16,39 @@ class Article(models.Model):
     # auto_now : 수정시마다 자동으로 저장
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(blank=True)
+    #image = models.ImageField(blank=True)
+    #image_thumbnail = ProcessedImageField(                                                          # processedImageField 말고 imageSpecField사용 할 경우 바로 이미지 사용(컷트 후) 가능
+    #     blank = True,
+    #     processors = [ResizeToFill(300, 300)],
+    #     format = 'JPEG',
+    #     options={'quality': 60}
+    # )
+    image_thumbnail = ImageSpecField(
+        processors = [ResizeToFill(300,300)],
+        format = 'JPEG',
+        options = {'QUALITY' : 80}
+    )                                                   #fill vs fit 
+                                                                        # fill : 300 300으로 자르기
+                                                                        # fit : 긴 쪽을 기준으로 하여 비율 유지하면서 자르기
+                                                                        #ProcessedImageField : Input 받은 것을 수정해 가면서 저장
+    image_fit = ProcessedImageField(
+        processor = [ResizeTofill(300, 300)],
+        format='JPEG',
+        options={'quality' : 80},
+        UPLOAD_TO='fit/'
+    )
+    image_fill = ProcessedImageField(
+        processor = [ResizeToFill(300, 300)],
+        format="JPEG",
+        options={'quality': 80}
+        
+    )
     def __str__(self):
         return f'<{self.id}> - <{self.title}>'
 # models.py : python 클래스 정의
             #:모델 설계도 
 # makemigrations : migration 파일 생성
 #                : DB 설계도 작성
-# migrate : migration 파일 DB 반영
 #      
 
 class Comment(models.Model):
